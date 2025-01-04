db0d5bf584d14e7f10b05a8c1e30992d38580dc9
Sohit Kumar <samratsohit@gmail.com>
2025-01-03 20:49:35 -0800

    loop commit

--- JS-1/index.js
+++ JS-1/index.js
@@ -1,8 +1,63 @@
 console.log('Hello World');
 
-let a = 5;
-console.log(a);
+// let a = 5;
+// console.log(a);
 
-a = 'Hello';
-console.log(a);
+// a = 'Hello';
+// console.log(a);
+
+// let Name = ['Rahul', 'Sahil', 'Rohan'];
+// console.log(Name[1]);
+
+
+// let b = 5;
+// let c = 10;
+// // console.log('value of b is:', ++b);
+
+// // console.log('value of b is:', b++);
+
+// console.log((++b) * (--c));
+// console.log((b++) * (c--));
+// console.log((++b) * (--c));
+
+
+// console.log(5!==6);
+
+// let age = 24;
+// let Status = (age >= 18) ? 'I can vote' : 'I cannot vote';
+// console.log(Status);
+
+// 
+
+// let num = 2;
+
+// switch(num){
+//     case 1:console.log('A');
+//     break;
+//     case 2:console.log('B');
+//     break;
+//     case 3:console.log('C');
+//     break;
+//     default:console.log('D');
+    
+// }
+
+// for(let i = 1; i <= 10; i=i+1){
+//     console.log(i);
+    
+// }
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i=i+1;
+// }
+
+
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i <= 10);
+
 
